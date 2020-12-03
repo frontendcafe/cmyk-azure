84a8b81b164257c5a02bf4bbed736e3a71ef123f
Agustin Vazquez <agustinvazquez453gmail.com>
2020-12-03 08:08:54 -0300

    import spotify button styles

--- src/components/SpotifyLoginButton.tsx
+++ src/components/SpotifyLoginButton.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {SpotifyAuth } from 'react-spotify-auth';
 import {CLIENT_ID, REDIRECT_URI} from '../services/spotify/config';
+import 'react-spotify-auth/dist/index.css'
 
 interface Props{
     scopes?:[]
