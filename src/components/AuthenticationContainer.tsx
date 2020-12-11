f5b94ce3966444af6aa61639d2494e2ac43ccc81
Agustin Vazquez <agustinvazquez453gmail.com>
2020-12-11 20:28:51 -0300

    configuro eslint y prettier

--- src/components/AuthenticationContainer.tsx
+++ src/components/AuthenticationContainer.tsx
@@ -1,32 +1,34 @@
 import React, { useState } from 'react';
 import Cookies from 'js-cookie';
-import { SpotifyApiContext } from 'react-spotify-api'
+import { SpotifyApiContext } from 'react-spotify-api';
 import SpotifyLoginButton from './SpotifyLoginButton';
-import {SpotifyAuthListener } from 'react-spotify-auth';
+import { SpotifyAuthListener } from 'react-spotify-auth';
 
-interface Props{
-    children: React.ReactNode
+interface Props {
+  children: React.ReactNode;
 }
 
-const AuthenticationContainer:React.FC<Props> = ({children}) => {
-    let initialToken = Cookies.get('spotifyAuthToken');
-    const [token, setToken] = useState<string>(initialToken ?? '');
+const AuthenticationContainer: React.FC<Props> = ({ children }) => {
+  let initialToken = Cookies.get('spotifyAuthToken');
+  const [token, setToken] = useState<string>(initialToken ?? '');
 
-    return (
-        <React.Fragment>
-            {
-                token ? (
-                    <SpotifyApiContext.Provider value={token} >
-                        {children}
-                    </SpotifyApiContext.Provider >
-                ) : (
-                        <SpotifyLoginButton />
-                    )
-            }
+  return (
+    <React.Fragment>
+      {token ? (
+        <SpotifyApiContext.Provider value={token}>
+          {children}
+        </SpotifyApiContext.Provider>
+      ) : (
+        <SpotifyLoginButton />
+      )}
 
-            <SpotifyAuthListener onAccessToken={(token: string) => { setToken(token); }} />
-        </React.Fragment>
-    );
+      <SpotifyAuthListener
+        onAccessToken={(token: string) => {
+          setToken(token);
+        }}
+      />
+    </React.Fragment>
+  );
 };
 
 export default AuthenticationContainer;
