c234e33a73b658e9d1dc5df776cd57cd432a1bc4
Agustin Vazquez <agustinvazquez453@gmail.com>
2020-12-15 16:39:01 -0300

    Agrego navbar e integro grid al layout

--- src/styles/variables.ts
+++ src/styles/variables.ts
@@ -2,8 +2,11 @@ export const PRIMARY_COLOR = 'rgb(75 178 252)';
 export const BACKGROUND_COLOR = '#0B0F18';
 export const DANGER_COLOR = '#dc3545';
 
+export const SECONDARY_FONT_FAMILY = 'Merriweather';
+
 export const TEXT_PRIMARY_COLOR = '#FFF';
 export const ICON_FONT_SIZE = '2rem';
+export const LONG_FONT_SIZE = '2rem';
 
 export const LIGHT_FONT_WEIGHT = '300';
 export const NORMAL_FONT_WEIGHT = '400';
