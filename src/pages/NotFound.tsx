33000fb725b6b9e1f82581ea2d4609b84dae1b1d
Agustin Vazquez <agustinvazquez453@gmail.com>
2020-12-14 22:02:00 -0300

    corro el comando format

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -2,11 +2,7 @@ import { RouteComponentProps } from '@reach/router';
 import React from 'react';
 
 const NotFound = (props: RouteComponentProps) => {
-  return (
-    <div>
-      NotFound
-    </div>
-  );
+  return <div>NotFound</div>;
 };
 
 export default NotFound;
