2db1cc1a4e837b22f2bf4de2f1a9fb9b2ed5335b
Agustin Vazquez <agustinvazquez453@gmail.com>
2020-12-18 10:57:33 -0300

    modificando formulario de recomendacion

--- src/components/RecomendationForm.tsx
+++ src/components/RecomendationForm.tsx
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import Playlist from '../models/Playlist';
+import { getActualUserPlaylists } from '../services/spotify/playlist';
 import { LIGHT_FONT_WEIGHT } from '../styles/variables';
+import ProfileIcon from './ProfileIcon';
 
 const StyledRecomendationForm = styled.form`
   font-weight: ${LIGHT_FONT_WEIGHT};
 `;
 
 const RecomendationForm = () => {
+  const [playlists, setPlaylists] = useState<Playlist[] | null>(null);
+
+  useEffect(() => {
+    getActualUserPlaylists()
+      .then(userPlaylists => setPlaylists(userPlaylists));
+
+  }, [])
+
   return (
     <StyledRecomendationForm>
+      <ProfileIcon />
       <p>SearchComponent</p>
       <p>PlaylistCardComponent</p>
       <p>ButtonSubmitComponent</p>
